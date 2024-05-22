6946ac8cb0f6d527e2fbeaa749f8db0d936b490f
devkelvim <devkelvimtairan@gmail.com>
2024-05-22 19:43:19 -0300

    estudando operadores logicos

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,22 +1,63 @@
 public class TiposVariaveis {
-    public static void main(String[] args) throws Exception {
-        double salarioMinimo = 2500;
+    /**
+     * //
+     */
+    //public static void main(String[] args) {
+        //int numero = 3;
+        //numero++;
+        //System.out.println(numero --);
+        //System.out.println(numero);
 
-        short numeroCurto = 1;
-        int numeroNormal = numeroCurto;
-        short numeroCurto2 = (short) numeroNormal;
 
-        String meuNome = "Kelvim Tairan";
-        System.out.println(meuNome);
+        //boolean variavel = true;
+        //System.out.println(!variavel);
+        //variavel = !variavel;
+        //System.out.println(variavel);
+    //public static void main(String[] args) {
 
-        int numero = 5;
-        numero = 10;
-        System.out.println(numero);
+        //int a, b;
+        //a = 6;
+        //b = 6;
+        //String resultado = a==b ?"verdadeiro" : "falso";
+        //System.out.println(resultado);
 
-        String nomeCompleto = "Linguagem " + "Java";
-        System.out.println(nomeCompleto);
+        //String nomeUm = "KELVIM";
+        //String nomeDois = new String("KELVIM");
+        //System.out.println(nomeUm.equals(nomeDois));
 
-        double soma = 10.5 + 10.5;
-        System.out.println(soma);
-    }
+        //int numero1 = 1;
+        //int numero2 = 2;
+
+        //boolean simNao = numero1 == numero2;
+        //if(numero1 < numero2){
+            //System.out.println("a nossa condição é verdadeira");
+        //}
+
+
+        //System.out.println("numeroUm é igual a numeroDois? " + simNao);
+
+        //simNao = numero1 != numero2;
+        //System.out.println("numeroUm é diferente de numeroDois? " + simNao);
+
+        //simNao = numero1 > numero2;
+        //System.out.println("numeroUm é maior que numeroDois? " + simNao);
+
+    //}
+        
+    //public static void main(String[] args) { //operadores lógicos
+        
+        //boolean condicao1 = true;
+        //boolean condicao2 = true;
+
+        //if(condicao1 && condicao2){
+            //System.out.println("as duas condicoes sao verdadeiras");
+        //}
+
+        //if(condicao1 || condicao2){
+            //System.out.println("Uma das condicoes é verdadeiras");
+        //}
+        //System.out.println("fim");
+    //}
+        
+
 }
